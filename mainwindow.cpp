58bdcf716808119c86b42e3a9bfc4778aef0a86e
Madhusudhan thapa <madhusudhant207@gmail.com>
2026-08-05 00:10:22 -0400

    Changes in the codebase

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,26 @@
 #include "mainwindow.h"
-
+#include <QMenuBar>
+#include <QMenu>
+#include <QPushButton>
 #include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QLabel *label = new QLabel("Hello Qt", this);
-    setCentralWidget(label);
-
+    menuBar()->addMenu("Files");//files menu
+    menuBar()->addMenu("Edit");//edit
+    menuBar()->addMenu("View");//view
+    QWidget *central = new QWidget(this);
+    setCentralWidget(central);
+    QLabel *label = new QLabel("Hello Qt");
+    QPushButton *button = new QPushButton("CLICK ME");//button
+    button->setFixedSize(200, 50);
+    QVBoxLayout *layout = new QVBoxLayout(central);
+    layout->addWidget(label);
+    layout->addWidget(button);
+    layout->setSpacing(20);
+    layout->setContentsMargins(20, 20, 20, 20);
     resize(900, 600);
 }
