32b30c02be7f8cee9d0b43d6000ce3fd247a2457
Madhusudhan thapa <madhusudhant207@gmail.com>
2026-08-04 23:57:28 -0400

    Changes in the codebase

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,33 +1,12 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include "settingswindow.h"
-#include <QDebug>
-#include <QFileDialog>
-#include <QMessageBox>
+
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    QLabel *label = new QLabel("Hello Qt", this);
+    setCentralWidget(label);
 
-    ui->textEdit_2->setPlaceholderText("Send a command/message...");
-    ui->textEdit_3->setPlaceholderText("initialize");
-
-    connect(
-        ui->actionopen_settings,
-        &QAction::triggered,
-        this,
-        [this]()
-        {
-            settingswindow *settings = new settingswindow(this);
-            settings->show();
-
-        }
-        );
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
+    resize(900, 600);
 }
